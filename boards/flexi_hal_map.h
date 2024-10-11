7a0f84412b0cf22aa9195fe6af9b463055df9854
engigeer <mitch.grams@gmail.com>
2024-10-11 08:16:57 -0600

    OnOffSpindleTweaks

--- boards/flexi_hal_map.h
+++ boards/flexi_hal_map.h
@@ -152,35 +152,40 @@
 #define AUXOUTPUT2_PIN          4
 #define AUXOUTPUT3_PORT         GPIOA
 #define AUXOUTPUT3_PIN          6
-#define AUXOUTPUT4_PORT         GPIOA // Spindle PWM
-#define AUXOUTPUT4_PIN          8
-#define AUXOUTPUT5_PORT         GPIOB // Spindle direction
-#define AUXOUTPUT5_PIN          1
-#define AUXOUTPUT6_PORT         GPIOB // Spindle enable
-#define AUXOUTPUT6_PIN          2
 
+
+#define AUXOUTPUT4_PORT         GPIOB // Spindle1 direction
+#define AUXOUTPUT4_PIN          15
+#define AUXOUTPUT5_PORT         GPIOB // Spindle1 enable
+#define AUXOUTPUT5_PIN          14
+#define AUXOUTPUT6_PORT         GPIOA // Spindle PWM
+#define AUXOUTPUT6_PIN          8
+#define AUXOUTPUT7_PORT         GPIOB // Spindle direction
+#define AUXOUTPUT7_PIN          1
+#define AUXOUTPUT8_PORT         GPIOB // Spindle enable
+#define AUXOUTPUT8_PIN          2
 
 // Define driver spindle pins
 #if DRIVER_SPINDLE_ENABLE
-#define SPINDLE_ENABLE_PORT     AUXOUTPUT6_PORT
-#define SPINDLE_ENABLE_PIN      AUXOUTPUT6_PIN
+#define SPINDLE_ENABLE_PORT     AUXOUTPUT8_PORT
+#define SPINDLE_ENABLE_PIN      AUXOUTPUT8_PIN
 #if DRIVER_SPINDLE_PWM_ENABLE
-#define SPINDLE_PWM_PORT        AUXOUTPUT4_PORT
-#define SPINDLE_PWM_PIN         AUXOUTPUT4_PIN
+#define SPINDLE_PWM_PORT        AUXOUTPUT6_PORT
+#define SPINDLE_PWM_PIN         AUXOUTPUT6_PIN
 #endif
 // Define spindle enable and spindle direction output pins.
-#define SPINDLE_DIRECTION_PORT  AUXOUTPUT5_PORT
-#define SPINDLE_DIRECTION_PIN   AUXOUTPUT5_PIN
+#define SPINDLE_DIRECTION_PORT  AUXOUTPUT7_PORT
+#define SPINDLE_DIRECTION_PIN   AUXOUTPUT7_PIN
 #endif //DRIVER_SPINDLE_ENABLE
 
-// Define driver spindle 1 pins (TEMPORARY ASSIGNMENT TO THE SAME PINS AS STEP/DIR FOR M4)
-#if DRIVER_SPINDLE1_ENABLE
-#define SPINDLE1_ENABLE_PORT    GPIOB
-#define SPINDLE1_ENABLE_PIN     14
-// Define spindle enable and spindle direction output pins.
-#define SPINDLE1_DIRECTION_PORT GPIOB
-#define SPINDLE1_DIRECTION_PIN  15
-#endif //DRIVER_SPINDLE_ENABLE
+// // Define spindle1 enable and direction output pins (TEMPORARY ASSIGNMENT TO THE SAME PINS AS STEP/DIR FOR M4)
+// #if DRIVER_SPINDLE1_ENABLE
+// #define SPINDLE1_ENABLE_PORT    AUXOUTPUT5_PORT
+// #define SPINDLE1_ENABLE_PIN     AUXOUTPUT5_PIN
+
+// #define SPINDLE1_DIRECTION_PORT AUXOUTPUT4_PORT
+// #define SPINDLE1_DIRECTION_PIN  AUXOUTPUT4_PIN
+// #endif //DRIVER_SPINDLE1_ENABLE
 
 // Define flood and mist coolant enable output pins.
 #define COOLANT_FLOOD_PORT      GPIOC
